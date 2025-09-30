ae85e64fd329a452c2361b7cd1c6ffdd41fd8a09
User <yuri.suser@gmail.com>
2025-09-30 13:45:07 +0200

    Debug Menu finisch

---
+++ My project/Assets/_Project/Debug/DebugViewer.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.InputSystem;
+using CoreClass = _Project.Core;   // алиас для удобного доступа к Core
+
+namespace _Project.Debug
+{
+    public class DebugViewer : MonoBehaviour
+    {
+        private static DebugViewer _instance;
+
+        [SerializeField] private TextMeshProUGUI[] lines = new TextMeshProUGUI[10];
+        [SerializeField] private GameObject panel;
+
+        private void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            // Очистим все строки при старте
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (lines[i] != null)
+                    lines[i].text = "";
+            }
+        }
+
+        private void Start()
+        {
+            if (panel != null)
+                panel.SetActive(false); // панель изначально выключена
+        }
+
+        private void OnEnable()
+        {
+            if (!Application.isPlaying) return;
+            CoreClass.Instance.Input.Subscribe(Key.F1, TogglePanel);
+        }
+
+        private void OnDisable()
+        {
+            if (!Application.isPlaying) return;
+            if (CoreClass.Instance != null)
+                CoreClass.Instance.Input.Unsubscribe(Key.F1, TogglePanel);
+        }
+
+        public void ShowMe(int num, string text, object val)
+        {
+            if (num < 0 || num >= lines.Length) return;
+            if (lines[num] != null)
+                lines[num].text = text + "\t" + val;
+        }
+
+        private void TogglePanel()
+        {
+            if (panel != null)
+                panel.SetActive(!panel.activeSelf);
+        }
+    }
+}
---
+++ My project/Assets/_Project/Inputs/InputController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+
+namespace _Project.Inputs
+{
+    public class InputController
+    {
+        private readonly Dictionary<Key, Action> _handlers = new();
+
+        public void Update()
+        { 
+            //UnityEngine.Debug.Log("VAR");
+            var kb = Keyboard.current;
+            if (kb == null) return;
+
+            foreach (var kvp in _handlers)
+            {
+                if (kb[kvp.Key].wasPressedThisFrame)
+                    kvp.Value?.Invoke();
+            }
+        }
+
+        public void Subscribe(Key key, Action handler)
+        {
+            if (!_handlers.ContainsKey(key))
+                _handlers[key] = null;
+
+            _handlers[key] += handler;
+        }
+
+        public void Unsubscribe(Key key, Action handler)
+        {
+            if (!_handlers.ContainsKey(key)) return;
+
+            _handlers[key] -= handler;
+            if (_handlers[key] == null)
+                _handlers.Remove(key);
+        }
+    }
+}
