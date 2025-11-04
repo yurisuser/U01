e301800bbc17d4fb3bcf5e9ceb9a851c242e566c
yurisuser <yuri.suser@gmail.com>
2025-11-04 18:00:56 +0100

    fraction upd

--- My project/Assets/_Project/Scripts/Runtime/NPC/Fraction/EFraction.cs
+++ My project/Assets/_Project/Scripts/Runtime/NPC/Fraction/EFraction.cs
@@ -2,11 +2,31 @@
 {
     public enum EFraction
     {
-        fraction1 = 0, // люди
-        fraction2 = 1, // органический рой
-        fraction3 = 2, // машины-создатели
-        fraction4 = 3, // номадские анклавы
-        fraction5 = 4, // архитекторы
-        fraction6 = 5  // симбионты
+        fraction0 = 0,
+        fraction1 = 1,
+        fraction2 = 2,
+        fraction3 = 3,
+        fraction4 = 4,
+        fraction5 = 5,
+        fraction6 = 6,
+        fraction7 = 7,
+        fraction8 = 8,
+        fraction9 = 9,
+        fraction10 = 10,
+        fraction11 = 11,
+        fraction12 = 12,
+        fraction13 = 13,
+        fraction14 = 14,
+        fraction15 = 15,
+        fraction16 = 16,
+        fraction17 = 17,
+        fraction18 = 18,
+        fraction19 = 19,
+        fraction20 = 20,
+        fraction21 = 21,
+        fraction22 = 22,
+        fraction23 = 23,
+        fraction24 = 24,
+        fraction25 = 25,
     }
 }
