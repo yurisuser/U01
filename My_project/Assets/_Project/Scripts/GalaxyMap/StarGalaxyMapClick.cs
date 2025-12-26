77c196cab197c8246309d109f54a40f61a76d761
yurisuser <yuri.suser@gmail.com>
2025-12-26 12:48:21 +0100

    upd1

--- My_project/Assets/_Project/Scripts/GalaxyMap/StarGalaxyMapClick.cs
+++ My_project/Assets/_Project/Scripts/GalaxyMap/StarGalaxyMapClick.cs
@@ -2,6 +2,7 @@ using _Project.Scripts.Core;
 using _Project.Scripts.Galaxy.Data;
 using _Project.Scripts.Core.Scene;
 using _Project.Scripts.UI;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -23,8 +24,11 @@ namespace _Project.Scripts.GalaxyMap.Runtime
 
         [Header("UI (панель звезды)")]
         [SerializeField] private GalaxyMapStarInfoController starInfoPanel;
+        [SerializeField] private float doubleClickThreshold = 0.25f;
 
         private Collider _col;
+        private float _lastLeftClickTime = -1f;
+        private Coroutine _singleClickRoutine;
 
         private void Awake()
         {
@@ -63,6 +67,59 @@ namespace _Project.Scripts.GalaxyMap.Runtime
 
         private void OnLeftClick()
         {
+            float timeSince = Time.time - _lastLeftClickTime;
+            if (timeSince <= doubleClickThreshold)
+            {
+                _lastLeftClickTime = -1f;
+                if (_singleClickRoutine != null)
+                {
+                    StopCoroutine(_singleClickRoutine);
+                    _singleClickRoutine = null;
+                }
+                OnDoubleLeftClick();
+                return;
+            }
+
+            _lastLeftClickTime = Time.time;
+            if (_singleClickRoutine != null)
+                StopCoroutine(_singleClickRoutine);
+            _singleClickRoutine = StartCoroutine(DelayedSingleClick());
+        }
+
+        private void OnRightClick()
+        {
+            if (System.HasValue)
+                starInfoPanel?.ShowStarInfo(System.Value.Uid);
+            else
+                starInfoPanel?.ClearStarInfo();
+
+            if (!logClick)
+                return;
+
+            int constellationId = System.HasValue ? System.Value.ConstellationId : -1;
+            Debug.Log($"[Star][ПКМ] {systemName} | ConstellationId={constellationId}");
+        }
+
+        private IEnumerator DelayedSingleClick()
+        {
+            yield return new WaitForSeconds(doubleClickThreshold);
+            _singleClickRoutine = null;
+            if (_lastLeftClickTime < 0f)
+                yield break;
+
+            OnSingleLeftClick();
+        }
+
+        private void OnSingleLeftClick()
+        {
+            if (System.HasValue)
+                starInfoPanel?.ShowStarInfo(System.Value.Uid);
+            else
+                starInfoPanel?.ClearStarInfo();
+        }
+
+        private void OnDoubleLeftClick()
+        {
             if (logClick)
             {
                 int constellationId = System.HasValue ? System.Value.ConstellationId : -1;
@@ -81,20 +138,6 @@ namespace _Project.Scripts.GalaxyMap.Runtime
             }
 
             SceneController.Load(SceneId.SystemMap);
-        }
-
-        private void OnRightClick()
-        {
-            if (System.HasValue)
-                starInfoPanel?.ShowStarInfo(System.Value.Uid);
-            else
-                starInfoPanel?.ClearStarInfo();
-
-            if (!logClick)
-                return;
-
-            int constellationId = System.HasValue ? System.Value.ConstellationId : -1;
-            Debug.Log($"[Star][ПКМ] {systemName} | ConstellationId={constellationId}");
         }
 
 #if UNITY_EDITOR
