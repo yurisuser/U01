bcebbf95f965253d234f59ef35d97a30f0656be3
yurisuser <yuri.suser@gmail.com>
2025-12-30 15:39:29 +0100

    Fraction upd

--- My_project/Assets/_Project/Classes/NPC/Fraction/Fractions.cs
+++ My_project/Assets/_Project/Classes/NPC/Fraction/Fractions.cs
@@ -5,12 +5,7 @@
         public static readonly Fraction[] All =
         {
             new Fraction(EFraction.fraction6, "Berserk"),  // Универсальный враг. атакует всех. атакован всеми. даже друг другом.
-            new Fraction(EFraction.fraction1, "fraction1"), // люди
-            new Fraction(EFraction.fraction2, "fraction2"), // органический рой
-            new Fraction(EFraction.fraction3, "fraction3"), // машины
-            new Fraction(EFraction.fraction4, "fraction4"), // номады
-            new Fraction(EFraction.fraction5, "fraction5"), // архитекторы
-            new Fraction(EFraction.fraction6, "fraction6")  // симбионты
+
         };
     }
 }
