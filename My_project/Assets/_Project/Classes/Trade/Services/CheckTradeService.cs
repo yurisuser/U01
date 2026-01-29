e18c9d0d35fd9af2aa9504b2178920e0fbd6a241
yurisuser <yuri.suser@gmail.com>
2026-01-29 19:58:52 +0100

    trade check service

---
+++ My_project/Assets/_Project/Classes/Trade/Services/CheckTradeService.cs
@@ -0,0 +1,4 @@
+namespace _Project.Scripts.Trade.Services
+{
+    public static class CheckTradeService { }
+}
---
+++ My_project/Assets/_Project/Classes/Trade/Services/TradeService.cs
@@ -0,0 +1,13 @@
+using _Project.Items;
+using _Project.Scripts.Ships;
+using _Project.Scripts.Stations;
+using _Project.Scripts.Trade.Models;
+
+namespace _Project.Scripts.Trade.Services
+{
+    /// <summary>Торговые операции (перенос товара) без участия денег у NPC.</summary>
+    public static class TradeService
+    {
+        // TODO: каркас будет заполнен после согласования универсальной модели торговли.
+    }
+}
